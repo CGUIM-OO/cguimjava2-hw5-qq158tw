6a5af321cc7211f23327bf9c44ac72f6e2dfe824
qq158tw <32104681+qq158tw@users.noreply.github.com>
2017-12-25 03:13:06 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 public class Player extends Person{
 	private String name;//玩家姓名
 	private int chips;//玩家有的籌碼
-	private int bet;//玩家此局下注的籌碼
-	private ArrayList<Card> oneRoundCard;//此牌局的卡
+	
 	
 	public Player(String name, int chips) //新增玩家
 	{
